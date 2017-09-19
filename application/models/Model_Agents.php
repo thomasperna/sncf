497bf22a4b2b317048ce7a1b1606961a06ac6708
Ivan Naluzhnyi <Ivan Naluzhnyi@PC-NALUZHNYI>
2017-09-19 11:55:00 +0200

    le tp2

--- application/models/Model_Agents.php
+++ application/models/Model_Agents.php
@@ -4,7 +4,7 @@ class Model_Agents extends CI_Model
     public function GetAllAgents(){
         $sql1 = $this->db->query("select code from formation");
          $sql = $this->db->query("select code from agent");
-       
+       //lol
        return $sql->result();
     }
 }
